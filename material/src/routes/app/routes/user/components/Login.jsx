7545040471f06a98b39b2c59914c275d5a55aeda
Robert DiNicolas <bob@roberts-mbp.lan>
2020-11-27 18:47:20 -0500

    fixes login issues

--- material/src/routes/app/routes/user/components/Login.jsx
+++ material/src/routes/app/routes/user/components/Login.jsx
@@ -11,9 +11,27 @@ const mapDispatchToProps = dispatch => ({
     }
 });
 
+const styles = {
+  submit: {
+    cursor: 'pointer',
+    color: 'rgb(78, 169, 243)',
+    backgroundColor: 'unset',
+    border: 'none',
+    fontSize: '18px',
+  }
+}
+
 class Login extends Component {
 
-    handleSubmit = () => {
+    constructor (props) {
+        super(props)
+
+        this.state = {};
+    }
+
+    handleSubmit = e => {
+
+        e.preventDefault();
 
         const { username, password } = this.state;
         const { login } = this.props;
@@ -21,10 +39,7 @@ class Login extends Component {
         login(username, password);
     }
 
-    setParam = e => {
-
-        this.setState({ [e.target.name]: e.target.value})
-    }
+    setParam = e => this.setState({ [e.target.name]: e.target.value});
 
     render () {
 
@@ -32,43 +47,43 @@ class Login extends Component {
             <div className="container-fluid no-breadcrumbs page-dashboard">
                 <div className="body-inner" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <div className="card" style={{ width: '50%', backgroundColor: 'rgba(0,0,0,0.21)', borderRadius: '5px', border: '1px solid rgb(46, 110, 115)' }}>
-                        <div className="card-content">
-                            <section className="logo text-center">
-                                <h4 style={{ color: '#5db7b4' }}>EHMLC Login</h4>
-                            </section>
-                            <form className="form-horizontal">
-                                <fieldset>
-                                    <div className="form-group">
-                                        <TextField
-                                            name="username"
-                                            onBlur={this.setParam}
-                                            floatingLabelText="Username"
-                                            floatingLabelFixed="true"
-                                            fullWidth
-                                        />
-                                    </div>
-                                    <div className="form-group">
-                                        <TextField
-                                            name="password"
-                                            onBlur={this.setParam}
-                                            floatingLabelText="Password"
-                                            floatingLabelFixed="true"
-                                            type="password"
-                                            fullWidth
-                                        />
-                                    </div>
-                                </fieldset>
-                            </form>
-                        </div>
-                        <div className="card-action no-border text-right">
-                            <a onClick={this.handleSubmit} className="color-primary" style={{ cursor: 'pointer', color: '#4ea9f3' }}>Login</a>
-                        </div>
+                        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                          <div className="card-content">
+                              <section className="logo text-center">
+                                  <h4 style={{ color: '#5db7b4' }}>EHMLC Login</h4>
+                              </section>
+                              <fieldset>
+                                  <div className="form-group">
+                                      <TextField
+                                          name="username"
+                                          onChange={this.setParam}
+                                          floatingLabelText="Username"
+                                          floatingLabelFixed="true"
+                                          fullWidth
+                                      />
+                                  </div>
+                                  <div className="form-group">
+                                      <TextField
+                                          name="password"
+                                          onChange={this.setParam}
+                                          floatingLabelText="Password"
+                                          floatingLabelFixed="true"
+                                          type="password"
+                                          fullWidth
+                                      />
+                                  </div>
+                              </fieldset>
+                          </div>
+                          <div className="card-action no-border text-right">
+                              <button type="submit" className="color-primary" style={styles.submit}>Login</button>
+                          </div>
+                        </form>
                     </div>
-                    <div className="additional-info">
+                    {/* <div className="additional-info">
                         <a href="#/app/register">Register</a>
                         <span className="divider-h" style={{ padding: '10px' }}> | </span>
                         <a href="#/forgot-password">Password Reset</a>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
